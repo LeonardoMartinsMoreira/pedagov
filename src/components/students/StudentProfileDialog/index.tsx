46d7543844710d77ddd1a4f2257781d171516f52
Leonardo Martins <leomartinsmoreira96@gmail.com>
2025-03-27 00:33:24 -0300

    feat: criado interface modal de perfil do aluno

--- src/components/students/StudentProfileDialog/index.tsx
+++ src/components/students/StudentProfileDialog/index.tsx
@@ -5,8 +5,8 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
-import { Label } from '@/components/ui/label'
-import { Textarea } from '@/components/ui/textarea'
+import { WhatsappLogo } from '@phosphor-icons/react/dist/ssr'
+import Image from 'next/image'
 
 export function StudentProfileDialog({
   isVisible,
@@ -22,16 +22,38 @@ export function StudentProfileDialog({
   return (
     <Dialog open={isVisible} onOpenChange={closeDialog}>
       <DialogContent className="flex flex-col gap-y-3 sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Perfil</DialogTitle>
+        <DialogHeader className="flex w-full flex-col items-center">
+          <DialogTitle className="text-center">
+            Leonardo Martins Moreira
+          </DialogTitle>
+
+          <Image
+            className="w-32 h-32 bg-black dark:bg-white rounded-full"
+            alt=""
+            src=""
+          />
+
+          <div className="flex w-full items-center justify-center gap-x-3">
+            <div className="flex flex-col w-24 justify-center items-center">
+              <span className="font-bold">3º TDS</span>
+              <span className="text-xs">Turma</span>
+            </div>
+
+            <div className="h-8 w-[1px] rounded bg-slate-600 dark:bg-white" />
+
+            <div className="flex flex-col w-24 justify-center items-center">
+              <span className="font-bold">3</span>
+              <span className="text-xs">Ocorrências</span>
+            </div>
+          </div>
         </DialogHeader>
-        <Label>Selecione o(s) Aluno(s)</Label>
-        <Label>Motivo</Label>
-        <Textarea
-          aria-describedby={undefined}
-          className="resize-none h-32"
-        ></Textarea>
-        <Button>Adicionar ocorrência</Button>
+
+        <div className="flex items-center w-full justify-center gap-x-2">
+          <Button className="w-full">Editar Perfil</Button>
+          <Button className="bg-green-500">
+            <WhatsappLogo className="text-white" />
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   )
