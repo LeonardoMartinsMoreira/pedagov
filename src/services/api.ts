dde88c142ecb3d5748c896b04f4d2c9901d792af
Leonardo Martins <leomartinsmoreira96@gmail.com>
2025-06-08 23:26:02 -0300

    ✨ feat: added interceptor for set authorization on header of request

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,20 @@
 import axios from 'axios'
+import { getSession } from 'next-auth/react'
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: 60000,
+  timeout: 30000,
+})
+
+api.interceptors.request.use(async (config) => {
+  const session = await getSession()
+  const accessToken = session?.user.accessToken
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`
+  }
+
+  return config
 })
 
 export { api }
